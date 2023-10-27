4cc928432062241ece6d4c6de6207b3385246e5b
renyuxuan1998 <ren.yuxu@northeastern.edu>
2023-10-26 23:38:15 -0700

    tests

--- Lab6/todo_app/tests/test_main.py
+++ Lab6/todo_app/tests/test_main.py
@@ -4,17 +4,28 @@ from src.main import app
 
 client = TestClient(app)
 
-def test_read_books():
+def test_create_book():
+    response = client.post("/books/", json={"name": "Test Book", "category": "fiction"})
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "name": "Test Book", "category": "fiction"}
+
+def test_get_books():
     response = client.get("/books/")
     assert response.status_code == 200
-    assert response.json() == {"message": "List of all books"}
+    assert response.json() == [{"id": 1, "name": "Test Book", "category": "fiction"}]
 
-def test_read_book_by_id():
+def test_get_book_by_id():
     response = client.get("/books/1")
     assert response.status_code == 200
-    assert response.json() == {"book_id": 1}
+    assert response.json() == {"id": 1, "name": "Test Book", "category": "fiction"}
 
-def test_read_books_by_category():
-    response = client.get("/books/by_category/?category=fiction")
+def test_update_book():
+    response = client.put("/books/1", json={"name": "Updated Book", "category": "non-fiction"})
     assert response.status_code == 200
-    assert response.json() == {"category": "fiction"}
+    assert response.json() == {"id": 1, "name": "Updated Book", "category": "non-fiction"}
+
+def test_delete_book():
+    response = client.delete("/books/1")
+    assert response.status_code == 200
+    assert response.json() == {"status": "success"}
+
